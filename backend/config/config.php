02b570554e84c84b0e1d5e0d73de1b8c58e2a1d2
chellsea30 <albanochellsea30@gmail.com>
2026-08-22 17:35:10 +0800

    config.php

--- backend/config/config.php
+++ backend/config/config.php
@@ -17,7 +17,7 @@
 define('DB_HOST', getenv('DB_HOST') ?: 'mysql.railway.internal');
 define('DB_USER', getenv('DB_USER') ?: 'root');
 define('DB_PASS', getenv('DB_PASSWORD') ?: 'uQcMTaYnsOvpMlVctbUHUtgBkAaryBWa');
-define('DB_NAME', getenv('DB_NAME') ?: 'tap_and_go_db');
+define('DB_NAME', getenv('DB_NAME') ?: 'railway');
 
 // Database connection settings
 define('DB_PORT', getenv('DB_PORT') ?: 3306);
